4a91faa686e0a1b2d39d0ccb446892dedfc16364
rithwanul <rithwanul@gmail.com>
2023-03-30 22:18:07 +0600

    10 - Using JdbcUserDetailsManager to perform authentication

--- src/main/java/com/tutorials/configs/WebSecurityConfig.java
+++ src/main/java/com/tutorials/configs/WebSecurityConfig.java
@@ -1,17 +1,16 @@
 package com.tutorials.configs;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,62 +18,96 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                .requestMatchers("/v1/api/welcome").permitAll()
-                .requestMatchers("/v1/api/auth/**").authenticated()
-                .requestMatchers("/v1/api/**").permitAll())
-                .httpBasic();
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.csrf(csrf -> {
+                        csrf.ignoringRequestMatchers("/v1/api/welcome", "/v1/api/**");
+                })
+                                .headers(headers -> headers.frameOptions().sameOrigin())
+                                .authorizeHttpRequests((authz) -> authz
+                                                .requestMatchers("/v1/api/welcome").permitAll()
+                                                .requestMatchers("/v1/api/auth/**").authenticated()
+                                                .requestMatchers("/v1/api/**").permitAll())
+                                .httpBasic();
 
-    /* @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("Admin")
-                .password("12345")
-                .roles("Admin")
-                .authorities(new SimpleGrantedAuthority("Admin"))
-                .build();
+                return http.build();
+        }
 
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("Don")
-                .password("12345")
-                .roles("User")
-                .authorities(new SimpleGrantedAuthority("User"))
-                .build();
+        /*
+         * @Bean
+         * public InMemoryUserDetailsManager userDetailsService() {
+         * UserDetails admin = User.withDefaultPasswordEncoder()
+         * .username("Admin")
+         * .password("12345")
+         * .roles("Admin")
+         * .authorities(new SimpleGrantedAuthority("Admin"))
+         * .build();
+         * 
+         * UserDetails user = User.withDefaultPasswordEncoder()
+         * .username("Don")
+         * .password("12345")
+         * .roles("User")
+         * .authorities(new SimpleGrantedAuthority("User"))
+         * .build();
+         * 
+         * return new InMemoryUserDetailsManager(admin, user);
+         * }
+         */
 
-        return new InMemoryUserDetailsManager(admin, user);
-    } */
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return NoOpPasswordEncoder.getInstance();
+        }
 
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-    }
-    
+        // @Bean
+        // public DataSource dataSource() {
+        //         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        //         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+        //         dataSourceBuilder.url("jdbc:mysql://localhost:3306/hibernatedemo");
+        //         dataSourceBuilder.username("root");
+        //         dataSourceBuilder.password("");
+        //         DataSource dataSource = dataSourceBuilder.build();
 
-     @Bean
-    public AuthenticationManager authenticationManagerBean(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http
-                .getSharedObject(AuthenticationManagerBuilder.class);
+        //         return dataSourceBuilder.build();
+        // }
 
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        UserDetails userAdmin = User.withUsername("Admin")
-        .password("12345").authorities("Admin").build();
-
-        UserDetails user = User.withUsername("User")
-                .password("12345").authorities("User").build();
-
-
-        manager.createUser(user);
-        manager.createUser(userAdmin);
-
-        authenticationManagerBuilder.userDetailsService(manager).passwordEncoder(passwordEncoder());
-        return authenticationManagerBuilder.build();
-    }
+        
 
 
+        @Bean
+        public DataSource dataSource() {
+                BasicDataSource dataSource = new BasicDataSource();
+                dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+                dataSource.setUrl("jdbc:mysql://localhost:3306/hibernatedemo");
+                dataSource.setUsername("root");
+                dataSource.setPassword("");
+                return dataSource;
+        }
+
+        // @Bean
+        // public AuthenticationManager authenticationManagerBean(HttpSecurity http)
+        // throws Exception {
+        // AuthenticationManagerBuilder authenticationManagerBuilder = http
+        // .getSharedObject(AuthenticationManagerBuilder.class);
+
+        // InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        // UserDetails userAdmin = User.withUsername("Admin")
+        // .password(passwordEncoder().encode("12345")).authorities("Admin").build();
+
+        // UserDetails user = User.withUsername("User")
+        // .password(passwordEncoder().encode("12345")).authorities("User").build();
+
+        // manager.createUser(user);
+        // manager.createUser(userAdmin);
+
+        // authenticationManagerBuilder.userDetailsService(userDetailsService())
+        // .passwordEncoder(passwordEncoder());
+        // return authenticationManagerBuilder.build();
+        // }
+
+        @Bean
+        public JdbcUserDetailsManager userDetailsService(DataSource dataSource) {
+           return new JdbcUserDetailsManager(dataSource);
+        }
 
 }
--- src/main/java/com/tutorials/springsecuritybasic/SpringsecuritybasicApplication.java
+++ src/main/java/com/tutorials/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,14 +2,15 @@ package com.tutorials.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
-@SpringBootApplication
 @ComponentScans({
 		@ComponentScan("com.tutorials.controllers"),
 		@ComponentScan("com.tutorials.configs")
 })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class SpringsecuritybasicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringsecuritybasicApplication.class, args);
